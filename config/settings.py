f68589ca946c0a7cfb0ad68b56779420e36aaeec
Emmanuel326 <nyariboemmanuel8@gmail.com>
2025-03-04 13:20:11 +0300

    Implement default ordering for Category model to fix pagination warning

--- config/settings.py
+++ config/settings.py
@@ -26,7 +26,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-3+#s68zck&dcp+^m--ttufd-q@fs$oy5-+n4@@^!2)l845vm@s'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['*']
 
